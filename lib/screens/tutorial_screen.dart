2b19df03802796b0bfe0da65530ef2e41a6f043a
wkkyu088 <dnjsrbwls08@gmail.com>
2023-01-10 01:22:38 +0900

    튜토리얼 페이지 초기

--- lib/screens/tutorial_screen.dart
+++ lib/screens/tutorial_screen.dart
@@ -29,7 +29,10 @@ class _TutorialScreenState extends State<TutorialScreen> {
 
     final double statusBarHeight = MediaQuery.of(context).padding.top;
     final double screenWidth = MediaQuery.of(context).size.width;
-    var items = [1, 2, 3, 4, 5].map((i) {
+    final double screenHeight = MediaQuery.of(context).size.height;
+    final double navigationBarHeight = MediaQuery.of(context).padding.bottom;
+
+    var items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) {
       return Builder(
         builder: (BuildContext context) {
           return Container(
@@ -45,18 +48,61 @@ class _TutorialScreenState extends State<TutorialScreen> {
 
     return Scaffold(
       backgroundColor: kWhite,
-      body: Column(
-        children: [
-          Container(
-            padding:
-                EdgeInsets.only(top: 10 + statusBarHeight, left: 10, right: 10),
-            width: screenWidth,
-            height: 100,
-            child: Stack(
-              children: [
-                Positioned(
-                  right: 0,
-                  child: TextButton(
+      body: SizedBox(
+        width: screenWidth,
+        height: screenHeight,
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.only(
+                  top: 20 + statusBarHeight, left: 10, right: 10),
+              width: screenWidth,
+              height: 100,
+            ),
+            Expanded(
+              child: Container(
+                width: screenWidth,
+                padding: const EdgeInsets.only(top: 30, bottom: 30),
+                child: CarouselSlider(
+                  items: items,
+                  carouselController: controller,
+                  options: CarouselOptions(
+                      aspectRatio: 9 / 16,
+                      viewportFraction: 1,
+                      initialPage: 0,
+                      enableInfiniteScroll: false,
+                      enlargeCenterPage: true,
+                      enlargeFactor: 0,
+                      onPageChanged: (index, reason) {
+                        setState(() {
+                          currentIdx = index;
+                        });
+                      }),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: items.asMap().entries.map((entry) {
+                      return GestureDetector(
+                        onTap: () => controller.animateToPage(entry.key),
+                        child: Container(
+                          width: 8,
+                          height: 8,
+                          margin: const EdgeInsets.symmetric(
+                              vertical: 8.0, horizontal: 5.0),
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: currentIdx == entry.key ? kMain : kGrey3),
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                  TextButton(
                     onPressed: () {
                       Navigator.pushAndRemoveUntil(
                         context,
@@ -67,66 +113,13 @@ class _TutorialScreenState extends State<TutorialScreen> {
                         (route) => false,
                       );
                     },
-                    child: Text("건너뛰기"),
-                  ),
-                ),
-                Center(
-                  child: Text(
-                    "title ${currentIdx + 1}",
-                    style: TextStyle(fontSize: kXL),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 30, bottom: 30),
-            child: CarouselSlider(
-              items: items,
-              carouselController: controller,
-              options: CarouselOptions(
-                  aspectRatio: 0.7,
-                  viewportFraction: 0.8,
-                  initialPage: 0,
-                  enableInfiniteScroll: false,
-                  enlargeCenterPage: true,
-                  enlargeFactor: 0.2,
-                  onPageChanged: (index, reason) {
-                    setState(() {
-                      currentIdx = index;
-                    });
-                  }),
-            ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Text(
-                "description ${currentIdx + 1}",
-                style: TextStyle(fontSize: kM),
+                    child: Text("건너뛰기", style: TextStyle(color: kGrey4)),
+                  )
+                ],
               ),
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: items.asMap().entries.map((entry) {
-              return GestureDetector(
-                onTap: () => controller.animateToPage(entry.key),
-                child: Container(
-                  width: 10.0,
-                  height: 10.0,
-                  margin: const EdgeInsets.symmetric(
-                      vertical: 8.0, horizontal: 5.0),
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: kGrey9
-                          .withOpacity(currentIdx == entry.key ? 0.9 : 0.4)),
-                ),
-              );
-            }).toList(),
-          ),
-          const SizedBox(height: 20),
-        ],
+          ],
+        ),
       ),
     );
   }
